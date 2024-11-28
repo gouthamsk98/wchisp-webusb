c032cc6fa3ca5dc9a52ffda4b506cd037d86abc1
gouthamsk98 <gouthamben10@gmail.com>
2024-11-28 15:05:55 +0530

    chore: add tsconfig for build and configure module export for dependency inclusion in other projects

--- flasher/index.js
+++ flasher/index.js
@@ -1,4 +1,4 @@
-export { default as CH_loader } from "./ch_loader";
-export { default as UsbTransport } from "./transport_handler";
-export { default as Protocol } from "./protocol_handler";
-export { default as ResponseHandler } from "./response_handler";
+export { CH_loader } from "./ch_loader";
+export { UsbTransport } from "./transport_handler";
+export { Protocol } from "./protocol_handler";
+export { ResponseHandler } from "./response_handler";
